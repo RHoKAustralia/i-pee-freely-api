fc646156f27d196af344e1457f853c40e0b2588e
Sean Dawson <contact@seandawson.info>
2015-12-05 20:08:51 +1100

    Added query for locations near a point.

--- i-pee-freely-api/common/models/location.js
+++ i-pee-freely-api/common/models/location.js
@@ -1,3 +1,28 @@
+var GeoPoint = require('loopback').GeoPoint;
+
 module.exports = function(Location) {
+    Location.nearTo = function(lat, lng, cb) {
+        console.log('-------------------------->');
+        console.log(lat);
+        console.log(lng);
+        console.log('-------------------------->');
+
+        var here = new GeoPoint({lat: lat, lng: lng});
+        Location.find( {where: {location: {near: here}}, limit: 3}, function(err, nearbyLocations) {
+            cb(err, nearbyLocations);
+        });
+    };
+    Location.remoteMethod(
+        'nearTo',
+        {
+          http: { path: '/near', verb: 'get' },
+          description: "Gets the locations near to a specified latitude and longitude.",
+          accepts: [
+            { arg: 'lat', type: 'number' },
+            { arg: 'lng', type: 'number' }
+          ],
+          returns: { arg: 'location', type: 'location' }
+        }
+  );
 
 };
